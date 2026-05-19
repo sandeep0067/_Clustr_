a0e64fad14f2dccc1b877392177c760308181a9b
sandeep0067 <sandeeppgsharp@gmail.com>
2026-05-19 10:31:55 +0530

    Serve static files from the 'dist' directory

--- backend/server.js
+++ backend/server.js
@@ -87,3 +87,10 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
